0a87bff32c7a88795a8e4ab74be0ea8846ad8987
Caitano <josiasmartinscaitano@gmail.com>
2022-04-18 16:54:58 -0300

    feature: implementado o controller da aplicação

---
+++ src/useCases/CreateUser/CreateUsersController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { CreateUsersUseCase } from './CreateUserUseCase';
+export class CreateUSerController {
+    constructor(
+        private createUserCase: CreateUsersUseCase
+    ) {}
+
+    async handler(request: Request, response: Response): Promise<Response> {
+        const { name, email, password } = request.body;
+
+      try {
+        await this.createUserCase.execute({
+            name,
+            email,
+            password
+        })
+
+        return response.status(201).send();
+      } catch (err) {
+          return response.status(400).json({
+              message: err.message || 'Unexpected error.'
+          })
+      }
+    }
+}
